e68882a77c197d8fc96dd3418bfc8d3a14f43d75
Justin <oglen.net@gmail.com>
2014-05-25 02:37:51 +0800

    modify

---
+++ app/data.js
@@ -0,0 +1,118 @@
+/**
+ * Created by Justin on 14-5-24.
+ */
+
+var testTree = {
+    id: 1,
+    data: 'node_1',
+    children: [
+        {
+            id: 2,
+            data: 'node_2',
+            children: [
+                {
+                    id: 4,
+                    data: 'node_4',
+                    children: [
+                        {
+                            id: 8,
+                            data: 'node_8',
+                            children: [
+                                {
+                                    id: 16,
+                                    data: 'node_16'
+                                },
+                                {
+                                    id: 17,
+                                    data: 'node_17'
+                                }
+                            ]
+                        },
+                        {
+                            id: 9,
+                            data: 'node_9',
+                            children: [
+                                {
+                                    id: 18,
+                                    data: 'node_18'
+                                },
+                                {
+                                    id: 19,
+                                    data: 'node_19'
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    id: 5,
+                    data: 'node_5',
+                    children: [
+                        {
+                            id: 10,
+                            data: 'node_10'
+                        },
+                        {
+                            id: 11,
+                            data: 'node_11'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            id: 3,
+            data: 'node_3',
+            children: [
+                {
+                    id: 6,
+                    data: 'node_6',
+                    children: [
+                        {
+                            id: 12,
+                            data: 'node_12'
+                        },
+                        {
+                            id: 13,
+                            data: 'node_13'
+                        }
+                    ]
+                },
+                {
+                    id: 7,
+                    data: 'node_7',
+                    children: [
+                        {
+                            id: 14,
+                            data: 'node_14',
+                            children: [
+                                {
+                                    id: 20,
+                                    data: 'node_20'
+                                },
+                                {
+                                    id: 21,
+                                    data: 'node_21'
+                                }
+                            ]
+                        },
+                        {
+                            id: 15,
+                            data: 'node_15',
+                            children: [
+                                {
+                                    id: 22,
+                                    data: 'node_22'
+                                },
+                                {
+                                    id: 23,
+                                    data: 'node_23'
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+};
